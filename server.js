0c59e3e34c3fb39e9d4c1de3ae2e37fe4a5c4243
vaibhavshukla <shukla.v@husky.neu.edu>
2018-08-10 12:00:05 -0400

    Allow local host and heroku

--- server.js
+++ server.js
@@ -10,8 +10,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin",
-        "https://evening-thicket-43962.herokuapp.com");
+    var allowedOrigins = ['https://evening-thicket-43962.herokuapp.com', 'http://localhost:4200'];
+    var origin = req.headers.origin;
+    if(allowedOrigins.indexOf(origin) > -1){
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
     res.header("Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods",
